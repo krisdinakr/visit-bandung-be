07ee0f6e9efff7c961518c2f0b5aeec495eea0e2
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-12-10 17:54:35 +0700

    add attraction route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import AttractionController from '../../controllers/attraction-controller';
+import ROUTES from '../index';
 
 const router = express.Router();
+
+router.get(ROUTES.ATTRACTIONS, AttractionController.getAll);
+router.get(ROUTES.ATTRACTIONS_SLUG, AttractionController.getBySlug);
 
 export default router;
