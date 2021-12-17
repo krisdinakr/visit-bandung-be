1867a143339a7da48f6b09978c9c603655bed3c5
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-12-17 14:30:34 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import api from './routes/api';
 import handleErrors from './middleware/handleErrors';
 
 dotenv.config();
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 app.use('/api', api);
 app.use(handleErrors);
